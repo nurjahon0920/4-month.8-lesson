e27bfe35143331f92c0bae666133f3e47ec1b3c6
Nurjahon <166979887+n20090920@users.noreply.github.com>
2024-05-09 06:48:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+//////////*String-1*//////////
+let s = 65;
+let str;
+let n = +prompt("n sonni kiriting__");
+for (let i = 0; i < n; i++) {
+  srt = str.codePointAt(s + i);
+  console.log(srt);
+}
+//////////*String-2*//////////
+//////////*String-3*//////////
+/*
+let s = prompt("String kiriting");
+let first = s[0];
+let second = s[s.length - 1];
+console.log(second.charCodeAt(`Oxirgi belgisi: ${second}`));
+console.log(first.charCodeAt(`Birinchi belgisi: ${first}`));
+*/
+//////////*String-4*//////////
+/*
+let A = prompt("A ning qiymatini kiriting ");
+let N = prompt("N sonini kiriting");
+console.log(A.repeat(N));
+*/
+//////////*String-5*//////////
+
+//////////*String-6*//////////
+//////////*String-7*//////////
+//////////*String-8*//////////
+//////////*String-9*//////////
+/*
+let str = prompt("String kiriting");
+let str2 = str.toLowerCase(str);
+console.log(str2);
+*/
+//////////*String-10*/////////
+//////////*String-11*/////////
+//////////*String-12*/////////
+//////////*String-13*/////////
+//////////*String-14*/////////
+//////////*String-15*/////////
+//////////*String-16*/////////
+//////////*String-17*/////////
+
+/*
+let str = prompt("String kiriting");
+let str2 = str.toLowerCase(str);
+console.log(str2);
+*/
+/*
+let s1 = "Hello world!";
+let s2 = "*";
+let c = "l";
+let newStr = s1.replaceAll(c, c + s2);
+console.log(newStr);
+*/
